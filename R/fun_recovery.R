de38ac342ed0bdcf8deb2233e31173f2c9340669
wandadk <wandadekeersmaecker@gmail.com>
2020-07-20 15:09:13 +0200

    improve documentation

--- R/fun_recovery.R
+++ R/fun_recovery.R
@@ -16,23 +16,27 @@ calcFrazier <- function(tsio, tdist, obspyr, shortDenseTS, nPre, nDist, nPostMin
   # check if there are enough observations before and after the disturbance to calculate the metrics
   if((tdist>((nPre*obspyr))) & (tdist < (length(tsio)-(nPostMax*obspyr)+1)) & (sum(!is.na(tsio))>2)){
     # translate parameters to those needed for the recovery functions
-    ys <- tsio
-    ts <- seq(1,length(tsio))
+    ys <- tsio # response
+    ts <- seq(1,length(tsio))# time
+
+    # disturbance dates
     if (obspyr == 1 | nDist == 0){
       tpert <- seq(tdist,tdist+(nDist*obspyr))
     }else{
       tpert <- seq(tdist,tdist+(nDist*obspyr)-1)
     }
+    # pre-disturbance period
     ts_pre <- seq(tdist-(nPre*obspyr),tdist-1)
-
+    # post-disturbance period
     if (obspyr == 1 | nPostMin == nPostMax){
       ts_post <-  seq(tdist +(nPostMin*obspyr), tdist +(nPostMax*obspyr))
     }else{
       ts_post <-  seq(tdist +(nPostMin*obspyr), tdist +(nPostMax*obspyr)-1)
     }
-
+    # timing between disturbance and post-disturbance state
     deltat <- switch(shortDenseTS + 1, (nPostMax*obspyr), ts_post-tdist)
 
+    # derive recovery indicators
     RRI <- rri(ts,ys,tpert,ts_pre, ts_post)
     R80P <- r80p(ts,ys,r = 0.8,ts_pre, ts_post)
     YrYr <- yryr(ts,ys,tpert, deltat)
@@ -52,15 +56,15 @@ calcFrazier <- function(tsio, tdist, obspyr, shortDenseTS, nPre, nDist, nPostMin
 #' @param tsio vector of observations (time series)
 #' @param obspyr number of observations in one year
 #' @param h This parameter defines the minimal segment size either given as fraction relative to the sample size or as an integer giving the minimal number of observations in each segment.
-#' @param shortDenseTS TRUE or FALSE. In case TRUE, the metrics are adjusted to be compatible with short, dense time series. In case FALSE, the input time series is assumed to have annual observations and at least 2 and 5 pre- and post-disturbance years, respectively.
-#' @param nPre If shortDenseTS is TRUE, number of years prior to the disturbance used to calculate the pre-disturbance value
-#' @param nDist If shortDenseTS is TRUE, number of months used to quantify the time series value during the disturbance
-#' @param nPostMin If shortDenseTS is TRUE, min number of years after the disturbance used to quantify the recovery
-#' @param nPostMax If shortDenseTS is TRUE, max number of years after the disturbance used to quantify the recovery
+#' @param shortDenseTS TRUE or FALSE. In case FALSE, the metrics follow closely the definitions given by Frazier et al
+#' @param nPre number of years prior to the disturbance used to calculate the pre-disturbance value
+#' @param nDist number of years used to quantify the time series value during the disturbance
+#' @param nPostMin min number of years after the disturbance used to quantify the recovery
+#' @param nPostMax max number of years after the disturbance used to quantify the recovery
 #' @param tdist the timing of the disturbance [observation number]
-#' @param maxBreak only for recovery indicators derived from segmented time series: if maxbreak is true, the maximum break in the segmented series is used as disturbance date to calculate the recovery indicators. If maxbreak is false, the break closest to the provided disturbance timing is used to calculate recovery.
+#' @param maxBreak only for recovery indicators derived from piecewise regression: if maxbreak is true, the maximum break in the segmented series is used as disturbance date to calculate the recovery indicators. If maxbreak is false, the break closest to the provided disturbance timing is used to calculate recovery.
+#' @param seas should a seasonal comonent be used in the piecewise regression?
 #' @param timeThres threshold on the duration between the disturbance date and date of the detected break [years]
-#' @param slpThres threshold on the pre-disturbance slope
 #'
 #' @return a list containing  the RRI, R80p, YrYr recovery indicator derived from the BFAST0n trend segments and slope of the trend segment after the disturbance (sl).
 #' @export
@@ -135,13 +139,13 @@ calcSegRec <- function(tsio, tdist, maxBreak, obspyr, h, shortDenseTS, nPre, nDi
         postChck <- ((trf[tbp+3] - trf[tbp+2]) > 0)
         # negative break
         distChck <- ((trf[tbp+1] - trf[tbp]) < 0)
-        # no negative break in recovery period with strong slope after the break
+        # no negative break in recovery period
         brkthres <- 1+(nPostMax*obspyr) #post-disturbance period used to assess recovery
         if(any((totbp>tbp) & (totbp<(brkthres+tbp)))){
           postbr <- totbp[(totbp>tbp) & (totbp<(brkthres+tbp))]
           postdbr <- trf[postbr+1]-trf[postbr]
-          postsl <- trf[postbr+3]-trf[postbr+2]
-          brkChck <- !any((postdbr<0) & (postsl>slpThres))#
+          # postsl <- trf[postbr+3]-trf[postbr+2]
+          brkChck <- !any((postdbr<0))# & (postsl>slpThres)
         }else{
           brkChck <- TRUE
         }
@@ -173,17 +177,17 @@ calcSegRec <- function(tsio, tdist, maxBreak, obspyr, h, shortDenseTS, nPre, nDi
 #' Calculate recovery for a single time series
 #'
 #' @param tsi vector: the first n values contain the timing of the disturbances and the next n values the observations for which the recovery indicators should be computed
-#' @param maxBreak (only for recovery indicators derived from segmented time series): if maxbreak is true, the maximum break in the segmented series is used as disturbance date to calculate the recovery indicators. If maxbreak is false, the break closest to the provided disturbance timing is used to calculate recovery.
+#' @param maxBreak (only for recovery indicators derived after piecewise regression): if maxbreak is true, the maximum break in the segmented series is used as disturbance date to calculate the recovery indicators. If maxbreak is false, the break closest to the provided disturbance timing is used to calculate recovery.
 #' @param obspyr the number of observations per year
-#' @param inp the preprocessing applied to the time series before computing the recovery indicators: segmented (for segmentations), smooth (time series smoothing using loess), or raw (no preprocessing)
-#' @param shortDenseTS if true, the recovery indicators are user designed, i.e. they can be adjusted to deal with short and/or dense time series
+#' @param inp the preprocessing applied to the time series before computing the recovery indicators: segmented (for piecewise regression), smooth (time series smoothing using loess), or raw (no preprocessing)
+#' @param shortDenseTS In case FALSE, the metrics follow closely the definitions given by Frazier et al
 #' @param nPre number of years prior to the disturbance that are used to derive the pre-disturbance condition
 #' @param nDist number of observations used to derive the disturbance state
 #' @param nPostMin start of the post-disturbance period: number of years after the disturbance
 #' @param nPostMax end of the post-disturbance period: number of years after the disturbance
 #' @param h h parameter of the breakpoints function in the strucchange package
-#' @param timeThres only relevant for segmentation: threshold on the duration between the disturbance date and date of the detected break [years]
-#' @param slpThres only relevant for segmentation:threshold on the pre-disturbance slope
+#' @param seas should a seasonal comonent be used in the piecewise regression?
+#' @param timeThres only relevant for piecewise regression: threshold on the duration between the disturbance date and date of the detected break [years]
 #'
 #' @return the RRI, R80P, YrYr and the slope of the pos-disturbance segment
 #' @export
@@ -197,24 +201,26 @@ calcRecoveryTS <- function(tsi, maxBreak, obspyr, inp = 'segmented', shortDenseT
   #   obspyr <- 1
   # }
   len <- length(tsi)
-  tdist <- tsi[1:(len/2)]
-  tsi <- tsi[(1+(len/2)):len]
+  tdist <- tsi[1:(len/2)]# data about the timing of the disturbance (0 equals no disturbance happened, 1 equals a disturbance took place)
+  tsi <- tsi[(1+(len/2)):len]# data about the vegetation response
 
   # Find the major break given by the disturbance dataset
-  tdist <- which(tdist == 1)
+  tdist <- which(tdist == 1)# get all dates where a disturbance took place
   if(length(tdist) > 0){
     dbr <- rep(NA,length(tdist))
     for(i  in 1:length(tdist)){
       if((tdist[i] > (2*obspyr)) & (tdist[i] < (length(tsi)-obspyr+1))){
+        # calculate magnitude break (difference between 2 years pre and 1 year post disturbance) for each disturbance date
         dbr[i] <- mean(tsi[(tdist[i]+1):(tdist[i]+(1*obspyr))], na.rm = T)-mean(tsi[(tdist[i]-2*obspyr):tdist[i]-1], na.rm = T)
       }else{
         dbr[i] <- 0
       }
     }
-    tdist <- tdist[which(abs(dbr) == max(abs(dbr)))]
+    tdist <- tdist[which(abs(dbr) == max(abs(dbr)))]# find largest break
     mval <- sum(is.na(tsi))/length(tsi) #fraction of missing values
 
     if (inp == 'smoothed'){
+      # smooth the time series using a loess filter
       df <- data.frame(dat = tsi,tm = 1:length(tsi))
       ls <- loess(dat  ~ tm, df, span = 0.2)#, span = 0.5
       tmps <- predict(ls, data.frame(tm = 1:length(tsi)), se = TRUE)
@@ -222,10 +228,12 @@ calcRecoveryTS <- function(tsi, maxBreak, obspyr, inp = 'segmented', shortDenseT
     }
 
     if((inp == 'smoothed') | (inp == 'raw')){
+      # calculate recovery indicators
       tmp <- calcFrazier(tsi, tdist, obspyr, shortDenseTS, nPre, nDist, nPostMin, nPostMax)
       outp <- c(tmp$RRI,tmp$R80P, tmp$YrYr, mval,NA,NA)
     }
     if(inp == 'segmented'){
+      # calculate recovery indicators after piecewise regression
       tmp <- calcSegRec(tsi, tdist, maxBreak, obspyr, h, shortDenseTS, nPre, nDist, nPostMin, nPostMax, timeThres, seas)
       outp <- c(tmp$RRI,tmp$R80P, tmp$YrYr, mval, tmp$loglik, tmp$AIC)
     }
@@ -236,10 +244,10 @@ calcRecoveryTS <- function(tsi, maxBreak, obspyr, inp = 'segmented', shortDenseT
 #' Calculate recovery indicators from a time series stack
 #'
 #' @param st raster stack, the first raster is a mask (pixels with value 0 are not considered, pixels with value 1 are included), the next n rasters represent disturbances (0 if no disturbance occours, 1 if a disturbance occurs) and the last n rasters contain the time series observations
-#' @param maxBreak (only for recovery indicators derived from segmented time series): if maxbreak is true, the maximum break in the segmented series is used as disturbance date to calculate the recovery indicators. If maxbreak is false, the break closest to the provided disturbance timing is used to calculate recovery.
+#' @param maxBreak (only for recovery indicators derived after piecewise regression): if maxbreak is true, the maximum break in the segmented series is used as disturbance date to calculate the recovery indicators. If maxbreak is false, the break closest to the provided disturbance timing is used to calculate recovery.
 #' @param obspyr the number of observations per year
-#' @param inp the preprocessing applied to the time series before computing the recovery indicators: segmented (for segmentations), smooth (time series smoothing using loess), or raw (no preprocessing)
-#' @param shortDenseTS if true, the recovery indicators are user designed, i.e. they can be adjusted to deal with short and/or dense time series
+#' @param inp the preprocessing applied to the time series before computing the recovery indicators: segmented (for piecewise regression), smooth (time series smoothing using loess), or raw (no preprocessing)
+#' @param shortDenseTS In case FALSE, the metrics follow closely the definitions given by Frazier et al
 #' @param nPre number of years prior to the disturbance that are used to derive the pre-disturbance condition
 #' @param nDist number of observations used to derive the disturbance state
 #' @param nPostMin start of the post-disturbance period: number of years after the disturbance
